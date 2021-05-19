8eeb8cc840994501ee095aeb53e314704c6d1c38
wnoyoung <nex5turbo@gmail.com>
2021-05-20 01:12:26 +0900

    프로토탑입대로 액티비티 생성 목금남음

--- app/src/main/java/com/example/myapplication2/activities/TempMainActivity.kt
+++ app/src/main/java/com/example/myapplication2/activities/TempMainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.myapplication2.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication2.R
 
-class SplashActivity : AppCompatActivity() {
+class TempMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        val intent = Intent(this, TempMainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_temp_main)
     }
 }
